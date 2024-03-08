98a644c0868997cda75503312b0238aaee5fa1a2
Saprigenie <Slava.paramonov.2002@yandex.ru>
2024-03-09 02:00:04 +0300

    feat: added session initialization

--- server/paths/nn.py
+++ server/paths/nn.py
@@ -1,11 +1,6 @@
 from flask_restx import Namespace, Resource
 from flask import abort, session
 
-from config import NN_NAMES
-from neural_nets.ann import ANN
-from neural_nets.cnn import CNN
-from neural_nets.gan import GAN
-
 
 api = Namespace("nn", description="Операции с нейронными сетями.")
 
@@ -13,16 +8,9 @@ api = Namespace("nn", description="Операции с нейронными се
 class ImagesList(Resource):
     def get(self, nn_name):
         if not session.get(nn_name):
-            if nn_name == NN_NAMES[0]:
-                session["model"] = ANN()
-            elif nn_name == NN_NAMES[1]:
-                session["model"] = CNN()
-            elif nn_name == NN_NAMES[2]:
-                session["model"] = GAN()
-            else:
-                abort(404)
+            abort(404)
+        else:
+            model = session.get(nn_name)
 
-
-        model = session.get("model") 
         structure = model.graph_structure()
         return structure
